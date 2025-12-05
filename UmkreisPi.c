d20012ccdacb00cf202d74cafa049f9c6c58c5bb
Emma Edl <EDL.Emma@htleisenstadt.at>
2025-12-05 11:06:03 +0100

    neu

--- UmkreisPi.c
+++ UmkreisPi.c
@@ -1,29 +1,27 @@
-#include <stdio.h>
-#include <math.h>
-
-int main() {
+ï»¿int main() {
     int n = 4;          // Start mit Quadrat
-    double r = 8.0;     // Kreisradius
-    double L, l, pi;
-    int iterations = 4; // Anzahl der Verdopplungen der Seiten
+    double r = 1.0;     // Kreisradius
+    double L, R, a, l, pi;
+    int iterations = 15; // Anzahl der Verdopplungen der Seiten
 
-    L = r * sqrt(2);    // Seite des eingeschriebenen Quadrats
+    L = 2 * r;    // Seite des eingeschriebenen Quadrats
+
 
     for (int i = 0; i < iterations; i++) {
-        // Länge der Seite des n-Seiten-Polygons
-        l = sqrt(2 * r * r - 2 * r * sqrt(r * r - (L / 2) * (L / 2)));
-
+        R = sqrt(r * r + L * L / 4);
+        a = R - r;
+        l = (L * L - 4 * a * a) / (2 * L);
         // Umfang
         double U = n * l;
 
-        // Pi-Annäherung
-        pi = 2*U / (2 * r);
+        // Pi-Annï¿½herung
+        pi = U / (r);
 
-        printf("n=%d, l=%.10f, Pi approx=%.10f\n", n, l, pi);
+        printf("n=%d, L=%.10f, R=%.10f, a=%.10f, l=%.10f, Pi approx=%.10f\n", n, L, R, a, l, pi);
 
         // Verdoppele die Anzahl der Seiten und halbieren die Polygon-Seite
         n *= 2;
-        L = l; // neue Seitenlänge für nächstes Polygon
+        L = l; // neue Seitenlï¿½nge fï¿½r nï¿½chstes Polygon
     }
 
     return 0;
